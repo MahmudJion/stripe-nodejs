2e7b5882558059b6f5ac4918821315cbb92745f9
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-07-13 00:14:03 +0100

    update js method

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
-const express = require('express')
-const bodyparser = require('body-parser')
-const path = require('path')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
-var Publishable_Key = 'Your_Publishable_Key'
-var Secret_Key = 'Your_Secret_Key'
+const Publishable_Key = 'Your_Publishable_Key';
+const Secret_Key = 'Your_Secret_Key';
 
-const stripe = require('stripe')(Secret_Key)
+const stripe = require('stripe')(Secret_Key);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(bodyparser.urlencoded({extended:false}))
-app.use(bodyparser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // View Engine Setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-	res.render('Home', {
-	key: Publishable_Key
-	})
-})
+app.get('/', function (req, res) {
+  res.render('Home', {
+    key: Publishable_Key
+  });
+});
 
-app.post('/payment', function(req, res){
+app.post('/payment', function (req, res) {
+  // Moreover, you can take more details from the user
+  // like Address, Name, etc from the form
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken,
+      name: 'Test User',
+      address: {
+        line1: 'abc',
+        postal_code: '452331',
+        city: 'test',
+        state: 'test',
+        country: 'test'
+      }
+    })
+    .then((customer) => {
+      return stripe.charges.create({
+        amount: 25000,
+        description: 'Web Development Product',
+        currency: 'USD',
+        customer: customer.id
+      });
+    })
+    .then((charge) => {
+      res.send('Success'); // If no error occurs
+    })
+    .catch((err) => {
+      res.send(err); // If some error occurs
+    });
+});
 
-	// Moreover you can take more details from user
-	// like Address, Name, etc from form
-	stripe.customers.create({
-		email: req.body.stripeEmail,
-		source: req.body.stripeToken,
-		name: 'Test User',
-		address: {
-			line1: 'abc',
-			postal_code: '452331',
-			city: 'test',
-			state: 'test',
-			country: 'test',
-		}
-	})
-	.then((customer) => {
-
-		return stripe.charges.create({
-			amount: 25000,
-			description: 'Web Development Product',
-			currency: 'USD',
-			customer: customer.id
-		});
-	})
-	.then((charge) => {
-		res.send("Success") // If no error occurs
-	})
-	.catch((err) => {
-		res.send(err)	 // If some error occurs
-	});
-})
-
-app.listen(port, function(error){
-	if(error) throw error
-	console.log("Server created Successfully")
-})
+app.listen(port, function (error) {
+  if (error) throw error;
+  console.log('Server created successfully');
+});
